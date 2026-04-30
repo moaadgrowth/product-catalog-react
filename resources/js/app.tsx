fbcb0f839a4166ff51768ecf0638c4fa3b9cbaf2
moaadgrowth <moa@adgrowth.se>
2026-04-30 10:00:35 +0200

    Update page titles

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,7 @@
 import { createInertiaApp } from '@inertiajs/react';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
 createInertiaApp({
-    title: (title) => (title ? `${title} - ${appName}` : appName),
+    title: (title) => title ?? '',
     progress: {
         color: '#4B5563',
     },
